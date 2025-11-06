a3f50bdce23bf6da6d1ebf6fb63a42e1b74ecf21
Prajwal J <prajwal.workmail@gmail.com>
2025-11-06 14:19:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "payper-suite-47747041-fadc6",
+  "appId": "1:634049395519:web:e45a6b820ae41a13faf18a",
+  "apiKey": "AIzaSyBjV1VZWboAfuAaHv-IpIfKNaGNZexKBfE",
+  "authDomain": "payper-suite-47747041-fadc6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "634049395519"
+};
